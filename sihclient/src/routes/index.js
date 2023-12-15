ee3f42425ce2b7a135d83a5343280a967e5b9181
AkhshayMahesh <106122010@nitt.edu>
2023-12-15 14:17:03 +0530

    options fix

--- sihclient/src/routes/index.js
+++ sihclient/src/routes/index.js
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Landing, Notfound, Login, Signup} from "../pages";
+import { Landing, Notfound, Login, Signup, Options} from "../pages";
 
 const router = createBrowserRouter([
   { path: "/", element: <Landing /> },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
+  { path: "/options", element: <Options /> },
   { path: "*", element: <Notfound /> },
 ]);
 
