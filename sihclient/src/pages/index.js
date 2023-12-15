2fe54f166037f2d783fd93a5f094f261ee1389b8
AkhshayMahesh <119474707+AkhshayMahesh@users.noreply.github.com>
2023-12-15 14:52:50 +0530

    options fix (#2)

--- sihclient/src/pages/index.js
+++ sihclient/src/pages/index.js
@@ -2,3 +2,5 @@ export {default as Landing} from "./Landing/Landing";
 export {default as Notfound} from "./Notfound/Notfound";
 export {default as Login} from "./login/login";
 export {default as Signup} from "./signup/signup";
+export {default as Options} from "./options/options";
+
